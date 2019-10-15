101f10b437e04ebc95d4d88e672276570b20e5e7
Chad Saglam <saglam.chd@gmail.com>
2019-10-15 14:51:25 +0200

    Added Loading Spinners and Re-Organized Backend Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -35,6 +37,7 @@ import { HeaderComponent } from './header/header.component';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    MatProgressSpinnerModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -14,6 +14,7 @@ export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
   post: Post;
+  isLoading = false;
   private mode = 'create';
   private postId: string;
 
@@ -26,12 +27,10 @@ export class PostCreateComponent implements OnInit {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
+        this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
-          this.post = {
-            id: postData._id,
-            title: postData.title,
-            content: postData.content
-          };
+          this.isLoading = false;
+          this.post = {id: postData._id, title: postData.title, content: postData.content};
         });
       } else {
         this.mode = 'create';
@@ -44,10 +43,15 @@ export class PostCreateComponent implements OnInit {
     if (form.invalid) {
       return;
     }
+    this.isLoading = true;
     if (this.mode === 'create') {
       this.postsService.addPost(form.value.title, form.value.content);
     } else {
-      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+      this.postsService.updatePost(
+        this.postId,
+        form.value.title,
+        form.value.content
+      );
     }
     form.resetForm();
   }
