e897d3f53e58bbedfd39743920608954688515bc
Chad Saglam <saglam.chd@gmail.com>
2019-10-16 17:03:21 +0200

    Fetching Posts Correctly

--- src/app/post/posts.service.ts
+++ src/app/post/posts.service.ts
@@ -10,7 +10,7 @@ import { Post } from './post.model';
 export class PostsService {
 
   private posts: Post[] = [];
-  private postsUpdated = new Subject<Post[]>();
+  private postsUpdated = new Subject<{ posts: Post[]; postCount: number }>();
 
   constructor(
     private http: HttpClient,
@@ -19,24 +19,29 @@ export class PostsService {
   getPosts(postsPerPage: number, currentPage: number) {
     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
     this.http
-      .get<{ message: string; posts: any; }>(
+      .get<{ message: string; posts: any; maxPosts: number }>(
         'http://localhost:3000/api/posts' + queryParams
       )
       .pipe(
         map(postData => {
-          return postData.posts.map(post => {
+          return { posts: postData.posts.map(post => {
             return {
               title: post.title,
               content: post.content,
               id: post._id,
               imagePath: post.imagePath
             };
-          });
+          }),
+          maxPosts: postData.maxPosts
+        };
         })
       )
-      .subscribe(transformedPosts => {
-        this.posts = transformedPosts;
-        this.postsUpdated.next([...this.posts]);
+      .subscribe(transformedPostData => {
+        this.posts = transformedPostData.posts;
+        this.postsUpdated.next({
+          posts: [...this.posts],
+          postCount: transformedPostData.maxPosts
+        });
       });
   }
 
