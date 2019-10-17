754d16b84ed32eb1b5ca3198a91ee9e8862bd899
Chad Saglam <saglam.chd@gmail.com>
2019-10-17 09:13:02 +0200

    Added the Signup view

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
+  isLoading = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSignup(form: NgForm) {
+    console.log(form.value);
+  }
 }
