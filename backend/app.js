ddd2cb0fbccd2368784ac065ecb75dbabaa7dea0
Chad Saglam <saglam.chd@gmail.com>
2019-10-18 00:34:12 +0200

    Sent the Token to the Frontend

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const postsRoutes = require("./routes/posts");
-const userRoutes = require("./routes/user");
+const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 mongoose
   .connect(
-    'mongodb+srv://Chad:8uOx1tHTNVfsmq27@mean-opysa.mongodb.net/node-angular',
+    'mongodb+srv://Chad:8uOx1tHTNVfsmq27@mean-opysa.mongodb.net/test',
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
@@ -22,17 +22,17 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/images', express.static(path.join('backend/images')));
+app.use('/images', express.static(path.join('backEnd/images')));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
   );
   next();
 });
