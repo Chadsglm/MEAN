eec991b537af34c41cb18db31997f1f9ae5d56fa
Chad Saglam <saglam.chd@gmail.com>
2019-10-20 13:30:31 +0200

    Protected Resources with Authorization

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -74,9 +74,18 @@ router.put(
       content: req.body.content,
       imagePath: imagePath
     });
-    console.log(post);
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
-      res.status(200).json({ message: 'Update successful!' });
+    // console.log(post);
+
+    Post.updateOne({
+      _id: req.params.id,
+      creator: req.userData.userId
+    }, post).then(result => {
+        // console.log(result);
+        if(result.nModified > 0) {
+          res.status(200).json({ message: 'Update successful!' });
+        } else {
+          res.status(401).json({ message: 'Not Authorized!' });
+        }
     });
   }
 );
@@ -116,11 +125,16 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.delete('/:id', checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({
-      message: 'Post deleted!'
-    });
+  Post.deleteOne({
+    _id: req.params.id,
+    creator: req.userData.userId
+   }).then(result => {
+    // console.log(result);
+    if(result.n > 0) {
+      res.status(200).json({ message: 'Deletion successful!' });
+    } else {
+      res.status(401).json({ message: 'Not Authorized!' });
+    }
   });
 });
 
