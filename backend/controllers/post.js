25e0bd58ddb36140ecaf576de373dd0c63f01260
Chad Saglam <saglam.chd@gmail.com>
2019-10-21 12:26:53 +0200

    Fixed update bug

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -48,7 +48,7 @@ exports.updatePost = (req, res, next) => {
   }, post)
   .then(result => {
     // console.log(result);
-    if(result.nModified > 0) {
+    if(result.n > 0) {
       res.status(200).json({ message: 'Update successful!' });
     } else {
       res.status(401).json({ message: 'Not Authorized!' });
